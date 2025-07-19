aa196bfda2dbaec2f2daccdd644c7063c75779bc
Darshi_Vinushika <dvinushika123@gmail.com>
2025-07-19 19:37:17 +0530

    preapred for the deployment

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -4,22 +4,35 @@ import { connectDB } from "./config/bd.js";
 import dotenv from "dotenv";
 import ratelimiter from "./middleware/rateLimiter.js";
 import cors from "cors";
+import path from "path";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
-
+const __dirname = path.resolve();
 
 //middleware
-app.use(cors({
-  origin: 'http://localhost:5173' // Adjust this to your frontend URL
-})
-);
+if(process.env.NODE_ENV !== "production") {
+  app.use(cors({
+    origin: 'http://localhost:5173' // Adjust this to your frontend URL
+  })
+  );
+}
 app.use(express.json());
 app.use(ratelimiter);
 
 app.use("/api/notes", notesRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../Frontend/dist")));
+  
+  app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../Frontend","dist", "index.html"));
+  });
+}
+
+
 
 //To connect the database before running the port
 connectDB().then(()=>{
